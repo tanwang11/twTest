ee280f73540d6ad2528f3bc397922aff4489edb1
艾慧勇 <377199761@qq.com>
2018-10-12 10:49:44 +0800

    创建

---
+++ twTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twTest
+//
+//  Created by HuXiaoyu on 2018/10/12.
+//  Copyright © 2018年 com.ifcar99. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
